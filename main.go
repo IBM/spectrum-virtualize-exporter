010d6f5c2618de20f4b0c173fcba1d8c3a46c15c
Mu Chen <chenmu@cn.ibm.com>
2021-04-30 09:50:29 +0800

    Disable metrics from the exporter itself by default
    Signed-off-by: Mu Chen <chenmu@cn.ibm.com>

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 	configFile             = kingpin.Flag("config.file", "Path to configuration file.").Default("spectrumVirtualize.yml").String()
 	metricsPath            = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
 	listenAddress          = kingpin.Flag("web.listen-address", "Address on which to expose metrics and web interface.").Default(":9119").String()
-	disableExporterMetrics = kingpin.Flag("web.disable-exporter-metrics", "Exclude metrics about the exporter itself (promhttp_*, process_*, go_*).").Bool()
+	disableExporterMetrics = kingpin.Flag("web.disable-exporter-metrics", "Exclude metrics about the exporter itself (promhttp_*, process_*, go_*).").Default("true").Bool()
 	// maxRequests            = kingpin.Flag("web.max-requests", "Maximum number of parallel scrape requests. Use 0 to disable.").Default("40").Int()
 	cfg             *utils.Config
 	enableCollector bool = true
